94449fcf3e2dca79efb9504ee2bd93ec3b3ea26a
Lee Boynton <lee@lboynton.com>
2013-02-11 12:34:21 +0000

    Fix syntax

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
 class r1soft_cdp_agent(
-    $key_server => undef,
+    $key_server = undef
 ) {
 	include r1soft_cdp_agent::repo
 	include r1soft_cdp_agent::packages
 
     exec { 'get-module':
-        command     => '/usr/bin/r1soft-setup --get-module --silent'
+        command     => '/usr/bin/r1soft-setup --get-module --silent',
 		subscribe   => Package['r1soft-cdp-enterprise-agent'],
 		refreshonly => true,
 	}
@@ -17,6 +17,6 @@ class r1soft_cdp_agent(
 	}
 
     if defined($key_server) {
-        r1soft_cdp_agent::key($key_server),
+        r1soft_cdp_agent::key($key_server)
     }
 }

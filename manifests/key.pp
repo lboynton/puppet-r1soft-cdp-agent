93688f36549e83ef163cd7be1afc5b30ab338d13
Lee Boynton <lee@lboynton.com>
2013-03-07 15:27:27 +0000

    Don't need explicit $name parameter

--- manifests/key.pp
+++ manifests/key.pp
@@ -1,4 +1,4 @@
-define r1soft_cdp_agent::key($name) {
+define r1soft_cdp_agent::key {
     exec { "/usr/bin/r1soft-setup --get-key ${name}":
         creates     => '/usr/sbin/r1soft/conf/server.allow/${name}',
         require     => Package['r1soft-cdp-enterprise-agent'],

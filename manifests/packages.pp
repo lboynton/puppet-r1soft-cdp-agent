6f56179fac8f8a02d03af6039780813c2798b9f2
Lee Boynton <lee@lboynton.com>
2013-03-26 15:48:02 +0000

    Fix formatting

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,23 +1,22 @@
 class r1soft_cdp_agent::packages {
     if $operatingsystem == ('redhat' or 'centos') {
-            if !defined(Package['kernel-devel']) {
-                package { 'kernel-devel': 
-                    ensure => installed,
-                    before => Package['r1soft-cdp-enterprise-agent'],
-                }
+        if !defined(Package['kernel-devel']) {
+            package { 'kernel-devel':
+                ensure => installed,
+                before => Package['r1soft-cdp-enterprise-agent'],
             }
+        }
 
-            if !defined(Package['kernel-headers']) {
-                package { 'kernel-headers': 
-                    ensure => installed,
-                    before => Package['r1soft-cdp-enterprise-agent'],
-                }
-            }    
+        if !defined(Package['kernel-headers']) {
+            package { 'kernel-headers':
+                ensure => installed,
+                before => Package['r1soft-cdp-enterprise-agent'],
+            }
+        }
     }
 
-
     package { 'r1soft-cdp-enterprise-agent':
-        ensure	=> installed,
-        require	=> Class['r1soft_cdp_agent::repo'],
+        ensure  => installed,
+        require => Class['r1soft_cdp_agent::repo'],
     }
 }

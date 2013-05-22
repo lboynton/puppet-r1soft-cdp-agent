edad89822e7d6c40ef8a079c5eeefd2bda9cfa63
drcursor <drcursor@ebserver.org>
2013-05-22 17:17:40 +0200

    Allow direct key definition

--- manifests/key.pp
+++ manifests/key.pp
@@ -1,9 +1,6 @@
 define r1soft_cdp_agent::key {
-    $server_filename = regsubst($name, '^(.*:)//([a-z0-9\-.]+)(:[0-9]+)?(.*)$','\2')
-    exec { "/usr/bin/r1soft-setup --get-key ${name}":
-        creates     => "/usr/sbin/r1soft/conf/server.allow/${server_filename}",
-        require     => Package['r1soft-cdp-enterprise-agent'],
-        logoutput   => on_failure,
+    file { "/usr/sbin/r1soft/conf/server.allow/key":
+        content => "${title}",
         notify      => Service['cdp-agent'],
     }
 }

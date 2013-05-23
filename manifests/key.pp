bcf1de9c016efeab572f842fd7631a4a5b9dd642
drcursor <drcursor@ebserver.org>
2013-05-23 09:33:39 +0200

    Install key only after cdp server is installed

--- manifests/key.pp
+++ manifests/key.pp
@@ -2,5 +2,6 @@ define r1soft_cdp_agent::key {
     file { "/usr/sbin/r1soft/conf/server.allow/key":
         content => "${title}",
         notify      => Service['cdp-agent'],
+        require     => Package['r1soft-cdp-enterprise-agent'],
     }
 }

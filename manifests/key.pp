e5f5e5d901770fcd97e9db06af1e439c549a5d6f
Lee Boynton <lee@lboynton.com>
2013-09-09 12:06:18 +0100

    Update package name in a few more places

--- manifests/key.pp
+++ manifests/key.pp
@@ -2,6 +2,6 @@ define serverbackup_cdp_agent::key {
     file { "/usr/sbin/r1soft/conf/server.allow/key":
         content => "${title}",
         notify      => Service['cdp-agent'],
-        require     => Package['serverbackup-cdp-enterprise-agent'],
+        require     => Package['serverbackup-enterprise-agent'],
     }
 }

4f67be84f2d258f3d59d46b05bfb4812844dbcbf
Anthony Somerset <anthony@somersettechsolutions.co.uk>
2013-09-09 12:42:35 +0200

    fixing bad apostrophes and package name

--- manifests/get_key.pp
+++ manifests/get_key.pp
@@ -2,7 +2,7 @@ define serverbackup_cdp_agent::get_key {
     $server_filename = regsubst($name, '^(.*:)//([a-z0-9\-.]+)(:[0-9]+)?(.*)$','\2')
     exec { "/usr/bin/serverbackup-setup --get-key ${name}":
         creates     => "/usr/sbin/r1soft/conf/server.allow/${server_filename}",
-        require     => Package[‘serverbackup-cdp-enterprise-agent'],
+        require     => Package['serverbackup-cdp-enterprise-agent'],
         logoutput   => on_failure,
         notify      => Service['cdp-agent'],
     }
--- manifests/packages.pp
+++ manifests/packages.pp
@@ -3,14 +3,14 @@ class serverbackup_cdp_agent::packages {
         if !defined(Package['kernel-devel']) {
             package { 'kernel-devel':
                 ensure => installed,
-                before => Package['serverbackup-cdp-enterprise-agent'],
+                before => Package['serverbackup-enterprise-agent'],
             }
         }
 
         if !defined(Package['kernel-headers']) {
             package { 'kernel-headers':
                 ensure => installed,
-                before => Package['serverbackup-cdp-enterprise-agent'],
+                before => Package['serverbackup-enterprise-agent'],
             }
         }
     }
@@ -20,11 +20,11 @@ class serverbackup_cdp_agent::packages {
         package { "linux-headers-${kernelrelease}":
             ensure  => installed,
             require => Class['serverbackup_cdp_agent::repo'],
-            before => Package['serverbackup-cdp-enterprise-agent'],
+            before => Package['serverbackup-enterprise-agent'],
         }
     }
 
-    package { 'serverbackup-cdp-enterprise-agent':
+    package { 'serverbackup-enterprise-agent':
         ensure  => installed,
         require => Class['serverbackup_cdp_agent::repo'],
     }

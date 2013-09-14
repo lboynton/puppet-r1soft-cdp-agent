90d00bb235c7cfb5d533fe277bb85277a9c98c12
Anthony Somerset <anthony@somersettechsolutions.co.uk>
2013-09-14 13:01:29 +0200

    adding Proxmox VE support to kernel-headers package

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -17,11 +17,21 @@ class serverbackup_cdp_agent::packages {
     
     if $operatingsystem == ('debian') {
     
-        package { "linux-headers-${kernelrelease}":
-            ensure  => installed,
-            require => Class['serverbackup_cdp_agent::repo'],
-            before => Package['serverbackup-enterprise-agent'],
-        }
+		if $kernelrelease  =~ /pve/ {
+	        package { "pve-headers-${kernelrelease}":
+            	ensure  => installed,
+            	require => Class['serverbackup_cdp_agent::repo'],
+            	before => Package['serverbackup-enterprise-agent'],
+        	}
+		}
+		else {
+
+			package { "linux-headers-${kernelrelease}":
+        	    ensure  => installed,
+        	    require => Class['serverbackup_cdp_agent::repo'],
+        	    before => Package['serverbackup-enterprise-agent'],
+        	}
+		}
     }
 
     package { 'serverbackup-enterprise-agent':

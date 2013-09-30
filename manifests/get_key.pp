f7fd92681db921d518b305778d5091fa0a7f2cdd
Lee Boynton <lee@lboynton.com>
2013-09-30 12:46:32 +0100

    Don't try to get key unless the module built

--- manifests/get_key.pp
+++ manifests/get_key.pp
@@ -2,7 +2,10 @@ define serverbackup_cdp_agent::get_key {
     $server_filename = regsubst($name, '^(.*:)//([a-z0-9\-.]+)(:[0-9]+)?(.*)$','\2')
     exec { "/usr/bin/serverbackup-setup --get-key ${name}":
         creates     => "/usr/sbin/r1soft/conf/server.allow/${server_filename}",
-        require     => Package['serverbackup-enterprise-agent'],
+        require     => [
+            Package['serverbackup-enterprise-agent'],
+            Exec['get-module'],
+        ],
         logoutput   => on_failure,
         notify      => Service['cdp-agent'],
     }

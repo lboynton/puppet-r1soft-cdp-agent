a543da8b60c99e3c8d468c5fbec16af02adc1f41
Lee Boynton <lee@lboynton.com>
2013-09-30 14:21:46 +0100

    Move comment to correct place

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,13 +11,13 @@ class serverbackup_cdp::params{
         default => $::key,
     }
 
-    # Since the top scope variable could be a string (if from an ENC), we might
-    # need to convert it to a boolean.
     $install_agent = $::install_agent ? {
         undef   => false,
         default => $::install_agent,
     }
 
+    # Since the top scope variable could be a string (if from an ENC), we might
+    # need to convert it to a boolean.
     if is_string($install_agent) {
         $safe_install_agent = str2bool($install_agent)
     } else {

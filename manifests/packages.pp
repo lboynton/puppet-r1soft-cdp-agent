0198fac6b216ed91e043c4d5dff256c225bf31cb
Lee Boynton <lee@lboynton.com>
2013-09-30 12:49:15 +0100

    Use switch instead of if statement to detect OS. Fixes bad check for OS.

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,35 +1,39 @@
 class serverbackup_cdp_agent::packages {
-    if $operatingsystem == ('redhat' or 'centos') {
-        if !defined(Package['kernel-devel']) {
-            package { 'kernel-devel':
-                ensure => installed,
-                before => Package['serverbackup-enterprise-agent'],
+    case $operatingsystem {
+        centos, redhat: {
+            if !defined(Package['kernel-devel']) {
+                package { 'kernel-devel':
+                    ensure => installed,
+                    before => Package['serverbackup-enterprise-agent'],
+                }
             }
-        }
 
-        if !defined(Package['kernel-headers']) {
-            package { 'kernel-headers':
-                ensure => installed,
-                before => Package['serverbackup-enterprise-agent'],
+            if !defined(Package['kernel-headers']) {
+                package { 'kernel-headers':
+                    ensure => installed,
+                    before => Package['serverbackup-enterprise-agent'],
+                }
             }
         }
-    }
-
-    if $operatingsystem == ('debian') {
-        # proxmox support
-        if $kernelrelease  =~ /pve/ {
-            package { "pve-headers-${kernelrelease}":
-                ensure  => installed,
-                require => Class['serverbackup_cdp_agent::repo'],
-                before => Package['serverbackup-enterprise-agent'],
+        debian, ubuntu: {
+            # proxmox support
+            if $kernelrelease  =~ /pve/ {
+                package { "pve-headers-${kernelrelease}":
+                    ensure  => installed,
+                    require => Class['serverbackup_cdp_agent::repo'],
+                    before => Package['serverbackup-enterprise-agent'],
+                }
+            }
+            else {
+                package { "linux-headers-${kernelrelease}":
+                    ensure  => installed,
+                    require => Class['serverbackup_cdp_agent::repo'],
+                    before => Package['serverbackup-enterprise-agent'],
+                }
             }
         }
-        else {
-            package { "linux-headers-${kernelrelease}":
-                ensure  => installed,
-                require => Class['serverbackup_cdp_agent::repo'],
-                before => Package['serverbackup-enterprise-agent'],
-            }
+        default: {
+            notify("Unrecognized operating system, you may need to install some kernel development packages yourself")
         }
     }
 
